d5f06d3c10348d98fa1e180d146feac0e73a6c7d
william2848 <27164@jcpstudents.org>
2024-09-25 10:24:43 -0500

    9/25/24
    DONE CRASHING OUT
    
    MEEPMEEP IS BACK

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -2,32 +2,34 @@ package com.example.meepmeeptesting;
 
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.noahbres.meepmeep.MeepMeep;
-import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
-import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
-import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class MeepMeepTesting {
-    public static void main(String[] args) {
+import org.rowlandhall.meepmeep.MeepMeep;
+import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
+import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-        MeepMeep meepMeep = new MeepMeep(800);
+    public class MeepMeepTesting {
+        public static void main(String[] args) {
+            MeepMeep meepMeep = new MeepMeep(800);
 
-        // Declare our first bot
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                // We set this bot to be blue
-                .setColorScheme(new ColorSchemeBlueDark())
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-
-                                .build()
-                );
-
-        // Declare out second bot
-
+            RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                    // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                    .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                    .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                            .forward(30)
+                            .turn(Math.toRadians(90))
+                            .forward(30)
+                            .turn(Math.toRadians(90))
+                            .forward(30)
+                            .turn(Math.toRadians(90))
+                            .forward(30)
+                            .turn(Math.toRadians(90))
+                            .build());
 
 
-        meepMeep.setDarkMode(true).setBackgroundAlpha(0.95f).addEntity(myBot).start();
+            meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
+                    .setDarkMode(true)
+                    .setBackgroundAlpha(0.95f)
+                    .addEntity(myBot)
+                    .start();
+        }
     }
-}
